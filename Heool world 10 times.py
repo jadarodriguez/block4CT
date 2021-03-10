23213f91c967158c7cb7826b277ab3625c70447f
jadarodriguez <79866953+jadarodriguez@users.noreply.github.com>
2021-03-10 09:45:59 -0700

    Add files via upload

---
+++ Heool world 10 times.py
@@ -0,0 +1,16 @@
+print("hello world")
+for x in range(0, 10):
+#basic for loop 
+for x in range(0, 10):
+    myList.append("apples")
+    print(mylist)
+    #basic list
+    myList = []
+    myList.append("pineapples")
+    x = input("Type a number")
+    print("That're more than nothing!")
+    else,:
+        #basic conditional
+        if x > 0:
+            x = int (x)
+print ("that's a small number")
