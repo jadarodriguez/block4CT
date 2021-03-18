066fc49a3d9128647bcce252af2a9ceb2010b6ac
jadarodriguez <79866953+jadarodriguez@users.noreply.github.com>
2021-03-18 11:17:00 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -7,19 +7,29 @@ Program goals:
 
 
 """
-inport random 
+import random
+myList = []
 def mainProgram():
     #build our while loop
     while True:
         print("Hello, there! let's work with lists")
-        print("please choose from the following options.  Type the number of choice")
-        choice = input("1. Add to list, 2. Return a value at a list   ")
+        print("please choose from the following options.  Type the number of the 
+              choice = input ("""1. Add to a list,
+2. Return a value in a list
+3. Add a bunch of numbers!
+4. Random search
+5. Print List
+6. Quit """)
             if choice == "1":
                 addToList()
             elif choice == "2":
                 indexValues()
             elif choice =="3":
                 randomSearch()
+            elif choice -- "4":
+                randomSearch()
+            elif choice --"5":
+                print (myList)
             else:
                 break
 
@@ -29,9 +39,23 @@ def addToList():
     myLIst.append (int(newItem))
     #we need to think about errors!
 
+
+def addABunch():
+    print("We're going to add a bunch of numbers to your list")
+    numToAdd = input("How many new integers would you like to add?  ")
+    numRange = input ("And how high would you likke these numbers to go?  ")
+    for x in range (0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+        print ("Your list is complete!")
+
 def randomSearch():
     print("oH IM sO rAnDom!!!")
     print myList[random.randint(0, len(myList)-1)])
+
+
+def linerSearch():
+    print ("We're going to go through this list one item at a time!")
+    
 
 def indexValues():
     print ("At what index position do you want to search")
