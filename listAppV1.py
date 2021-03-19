7a31c0bad0cbca029f7461c3665f49623436f9e0
jadarodriguez <79866953+jadarodriguez@users.noreply.github.com>
2021-03-19 11:08:11 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -13,24 +13,27 @@ def mainProgram():
     #build our while loop
     while True:
         print("Hello, there! let's work with lists")
-        print("please choose from the following options.  Type the number of the 
-              choice = input ("""1. Add to a list,
+        print("please choose from the following options.  Type the number of the ")
+        choice = input ("""1. Add to a list,
 2. Return a value in a list
 3. Add a bunch of numbers!
-4. Random search
-5. Print List
-6. Quit """)
-            if choice == "1":
-                addToList()
-            elif choice == "2":
-                indexValues()
-            elif choice =="3":
-                randomSearch()
-            elif choice -- "4":
-                randomSearch()
-            elif choice --"5":
-                print (myList)
-            else:
+4. Random Search
+5. Liner Search
+6. Print List
+7. Quit """)
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+             indexValues()
+        elif choice =="3":
+             randomSearch()
+        elif choice -- "4":
+             randomSearch()
+        elif choice --"5":
+             linerSearch ()
+        elif choice =="6":
+            print (myList)
+        else:
                 break
 
 def addToList():
@@ -50,12 +53,15 @@ def addABunch():
 
 def randomSearch():
     print("oH IM sO rAnDom!!!")
-    print myList[random.randint(0, len(myList)-1)])
+    print(myList[random.randint(0, len(myList)-1)])
 
 
 def linerSearch():
     print ("We're going to go through this list one item at a time!")
-    
+    searchValue = input("What are you looking for?  ")
+    for x in range (len(myList)):
+        if myList[x] == int(searchValue):
+            print ("Your item is at index position {}".format(x))
 
 def indexValues():
     print ("At what index position do you want to search")
