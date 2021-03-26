0c9e5866951397b7adbd9e077550c3708517de7a
jadarodriguez <79866953+jadarodriguez@users.noreply.github.com>
2021-03-26 11:02:06 -0600

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,9 @@
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x*factorial(x-1))
+
+if __name__ == "__main__":
+     num = input ("What number would you like the factoraial of?  ")
+     print ("The factorial of", num, "is", factorial(int(num))
